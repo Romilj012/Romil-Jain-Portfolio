6b4c595e2f409f0d367ba85b8623bfe4af140795
Romilj012 <romilj012@gmail.com>
2025-01-29 21:43:32 -0600

    added resume

--- app/api/download-resume/route.ts
+++ app/api/download-resume/route.ts
@@ -4,13 +4,13 @@ import path from "path"
 
 export async function GET() {
   try {
-    const filePath = path.join(process.cwd(), "public", "resume.pdf")
+    const filePath = path.join(process.cwd(), "public", "Jain_Romil.pdf")
     const fileBuffer = fs.readFileSync(filePath)
 
     return new NextResponse(fileBuffer, {
       headers: {
         "Content-Type": "application/pdf",
-        "Content-Disposition": "attachment; filename=romil-jain-resume.pdf",
+        "Content-Disposition": "attachment; filename=Jain_Romil.pdf",
       },
     })
   } catch (error) {
